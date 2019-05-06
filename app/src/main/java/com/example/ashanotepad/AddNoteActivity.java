0c1aceee64357c42e7fcd47227dd127226278df5
Asha <asherally079@gmail.com>
2019-05-06 19:19:50 +0300

    first java code

---
+++ app/src/main/java/com/example/ashanotepad/AddNoteActivity.java
@@ -0,0 +1,42 @@
+package com.example.ashanotepad;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AddNoteActivity extends AppCompatActivity {
+    EditText etTitle;
+    EditText etNote;
+    Button btnAddPhoto;
+    Button btnAddVoiceNote;
+    Button btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        etTitle=findViewById(R.id.etTitle);
+        etNote=findViewById(R.id.etNote);
+        btnAddPhoto=findViewById(R.id.btnAddPhoto);
+        btnAddVoiceNote=findViewById(R.id.btnAddVoiceNote);
+        btnSave=findViewById(R.id.btnSave);
+
+
+        }}
+
+
+
+
+
